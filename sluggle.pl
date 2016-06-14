0a26d03402c1a261a2ec32357c2072f68d3f0691
chrisjrob <chrisjrob@gmail.com>
2016-06-14 10:46:49 +0000

    Change UA to prevent 403 forbidden - issue #21

--- sluggle.pl
+++ sluggle.pl
@@ -830,6 +830,7 @@ sub get_data {
     $ua->timeout(20);
     $ua->protocols_allowed( [ 'http', 'https'] );
     $ua->max_size(1024 * 1024 * 8);
+    $ua->agent('Mozilla/5.0');
     $ua->env_proxy;
 
     my $response = $ua->get($query);
